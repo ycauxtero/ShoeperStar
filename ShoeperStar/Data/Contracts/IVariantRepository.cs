4a06f544e0208d94c737272c84bad45780281ffb
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:16:13 +0800

    created the variant repository interface

---
+++ ShoeperStar/Data/Contracts/IVariantRepository.cs
@@ -0,0 +1,14 @@
+﻿using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface IVariantRepository
+    {
+        Task<IEnumerable<Variant>> GetAllVariants(bool trackChanges, bool includeNavigationFields = false);
+        Task<Variant> GetVariantAsync(int variantId, bool trackChanges, bool includeNavigationFields = false);
+        Task<IEnumerable<Variant>> GetVariantsByShoeIdAsync(int shoeId, bool trackChanges, bool includeNavigationFields = false);
+        void CreateVariant(Variant variant);
+        void DeleteVariant(Variant variant);
+        void UpdateVariant(Variant variant);
+    }
+}
