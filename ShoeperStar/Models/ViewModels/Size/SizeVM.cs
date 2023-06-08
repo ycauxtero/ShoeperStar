1fa880f2fb3c226312e81dac3c9929c3a6912469
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:49:16 +0800

    created base size view model

---
+++ ShoeperStar/Models/ViewModels/Size/SizeVM.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoeperStar.Models.ViewModels.Size
+{
+    public class SizeVM
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public int Quantity { get; set; }
+        public string SKU { get; set; }
+    }
+}
