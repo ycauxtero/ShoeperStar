c3a0c5047ca5c727bb6f48342ec464c69848967d
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:21:09 +0800

    created the repository manager interface

---
+++ ShoeperStar/Data/Contracts/IRepositoryManager.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoeperStar.Data.Contracts
+{
+    public interface IRepositoryManager
+    {
+        IBrandRepository Brands { get; }
+        IGenderRepository Genders { get; }
+        ICategoryRepository Categories { get; }
+        IShoeRepository Shoes { get; }
+        IVariantRepository Variants { get; }
+        ISizeRepository Sizes { get; }
+        Task SaveAsync();
+    }
+}
