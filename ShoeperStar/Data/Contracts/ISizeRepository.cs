7c2a47c68cbacbdeec283d9059564d26ecb5f99e
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:16:50 +0800

    created the size repository interface

---
+++ ShoeperStar/Data/Contracts/ISizeRepository.cs
@@ -0,0 +1,15 @@
+﻿using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface ISizeRepository
+    {
+        Task<IEnumerable<Size>> GetAllSizes(bool trackChanges, bool includeNavigationFields = false);
+        Task<Size> GetSizeAsync(int sizeId, bool trackChanges, bool includeNavigationFields = false);
+        Task<IEnumerable<Size>> GetSizesByVariantIdAsync(int variantId, bool trackChanges, bool includeNavigationFields = false);
+        void CreateSize(Size size);
+        void DeleteSize(Size size);
+        void UpdateSize(Size size);
+        void UpdateSizes(IEnumerable<Size> sizes);
+    }
+}
