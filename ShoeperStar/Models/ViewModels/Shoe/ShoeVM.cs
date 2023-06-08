4ef60d1a0ea55de81e44a529b5594a4307eb93a3
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:50:22 +0800

    create shoe view model (complete properties)

---
+++ ShoeperStar/Models/ViewModels/Shoe/ShoeVM.cs
@@ -0,0 +1,23 @@
+﻿using ShoeperStar.Models.ViewModels.Variant;
+
+namespace ShoeperStar.Models.ViewModels.Shoe
+{
+    public class ShoeVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+
+        public int BrandId { get; set; }
+        public string BrandName { get; set; }
+
+        public int GenderId { get; set; }
+        public string GenderName { get; set; }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public ICollection<ShoeVariantVM> Variants { get; set; }
+    }
+}
