502666f4ff78d847e89ddd393eb68122a94b6bb0
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:15:45 +0800

    implemented the gender repository interface

---
+++ ShoeperStar/Data/Repository/GenderRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoeperStar.Data.Contracts;
+using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Repository
+{
+    public class GenderRepository : RepositoryBase<Gender>, IGenderRepository
+    {
+        public GenderRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public void CreateGender(Gender gender)
+        {
+            Create(gender);
+        }
+
+        public void DeleteGender(Gender gender)
+        {
+            Delete(gender);
+        }
+
+        public async Task<IEnumerable<Gender>> GetAllGenders(bool trackChanges)
+        {
+            return await FindAll(trackChanges).ToListAsync();
+        }
+
+        public async Task<Gender> GetGenderAsync(int genderId, bool trackChanges)
+        {
+            return await FindByCondition(x => x.Id == genderId, trackChanges).SingleOrDefaultAsync();
+        }
+
+        public void UpdateGender(Gender gender)
+        {
+            Update(gender);
+        }
+    }
+}
