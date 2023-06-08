6c8cbe87108c46a8b1e69469dfb2a363d0f8e45b
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:59:12 +0800

    create mapping from shoe to shoeVM

--- ShoeperStar/MappingProfile.cs
+++ ShoeperStar/MappingProfile.cs
@@ -1,12 +1,20 @@
 ﻿using AutoMapper;
+using ShoeperStar.Models.ViewModels.Shoe;
+using ShoeperStar.Models;
 
 namespace ShoeperStar
 {
     public class MappingProfile : Profile
     {
         public MappingProfile() 
-        { 
-        
+        {
+            //Entity to EntityVM
+            CreateMap<Shoe, ShoeVM>()
+                .ForMember(s => s.BrandName, opt => opt.MapFrom(x => x.Brand.Name))
+                .ForMember(s => s.CategoryName, opt => opt.MapFrom(x => x.Catergory.Name))
+                .ForMember(s => s.GenderName, opt => opt.MapFrom(x => x.Gender.Name))
+                .ForMember(s => s.Variants, opt => opt.MapFrom(x => x.Variants));
+
         }
     }
 }
