7ade24ffa3cbfb803bc0f6bd12f7c56cd003bacc
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:15:25 +0800

    created the gender repository interface

---
+++ ShoeperStar/Data/Contracts/IGenderRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface IGenderRepository
+    {
+        Task<IEnumerable<Gender>> GetAllGenders(bool trackChanges);
+        Task<Gender> GetGenderAsync(int genderId, bool trackChanges);
+        void CreateGender(Gender gender);
+        void DeleteGender(Gender gender);
+        void UpdateGender(Gender gender);
+    }
+}
