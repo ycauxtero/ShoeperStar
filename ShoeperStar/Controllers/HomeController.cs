c087fe907ccba2074abc2c47af36b3494f12f560
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:57:28 +0800

    implemented the default Index method

--- ShoeperStar/Controllers/HomeController.cs
+++ ShoeperStar/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ShoeperStar.Data.Contracts;
 using ShoeperStar.Models;
+using ShoeperStar.Models.ViewModels.Shoe;
 using System.Diagnostics;
 
 namespace ShoeperStar.Controllers
@@ -17,9 +18,13 @@ namespace ShoeperStar.Controllers
             _mapper = mapper;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // CHANGE IMPLEMENTATION DEPENDING ON WHAT TO SHOES TO DISPLAY
+            var shoesToDisplay = await _repositoryManager.Shoes.GetAllShoes(trackChanges: false, includeNavigationFields: true);
+            var shoesToisplayVM = _mapper.Map<IEnumerable<ShoeVM>>(shoesToDisplay);
+
+            return View(shoesToisplayVM);
         }
 
     }
