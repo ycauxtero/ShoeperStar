cecc649f0ae3a995a0a8976a9fbdc2ead1de3ad2
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:14:02 +0800

    created the brand repository interface

---
+++ ShoeperStar/Data/Contracts/IBrandRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface IBrandRepository
+    {
+        Task<IEnumerable<Brand>> GetAllBrands(bool trackChanges);
+        Task<Brand> GetBrandAsync(int brandId, bool trackChanges);
+        void CreateBrand(Brand brand);
+        void DeleteBrand(Brand brand);
+        void UpdateBrand(Brand brand);
+    }
+}
