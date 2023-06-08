8666f99b2f24a9f51e1a66177b7448f4d5dceacc
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:17:20 +0800

    created the shoe repository interface

---
+++ ShoeperStar/Data/Contracts/IShoeRepository.cs
@@ -0,0 +1,14 @@
+﻿using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface IShoeRepository
+    {
+        Task<IEnumerable<Shoe>> GetAllShoes(bool trackChanges, bool includeNavigationFields = false);
+        Task<Shoe> GetShoeAsync(int shoeId, bool trackChanges, bool includeNavigationFields = false);
+        void CreateShoe(Shoe shoe);
+        Task<IEnumerable<Shoe>> GetByIds(IEnumerable<int> ids, bool trackChanges, bool includeNavigationFields = false);
+        void DeleteShoe(Shoe shoe);
+        void UpdateShoe(Shoe shoe);
+    }
+}
