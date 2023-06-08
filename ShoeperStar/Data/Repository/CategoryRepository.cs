047b7aabdb39176737d45911fccfe25f37af5324
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:15:07 +0800

    implemented the category repository interface

---
+++ ShoeperStar/Data/Repository/CategoryRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoeperStar.Data.Contracts;
+using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Repository
+{
+    public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+    {
+        public CategoryRepository(AppDbContext context) : base(context)
+        {
+        }
+        public void CreateCatergory(Category catergory)
+        {
+            Create(catergory);
+        }
+
+        public void DeleteCatergory(Category catergory)
+        {
+            Delete(catergory);
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCatergories(bool trackChanges)
+        {
+            return await FindAll(trackChanges).ToListAsync();
+        }
+
+        public async Task<Category> GetCatergoryAsync(int catergoryId, bool trackChanges)
+        {
+            return await FindByCondition(x => x.Id == catergoryId, trackChanges).SingleOrDefaultAsync();
+        }
+
+        public void UpdateCatergory(Category catergory)
+        {
+            Update(catergory);
+        }
+    }
+}
