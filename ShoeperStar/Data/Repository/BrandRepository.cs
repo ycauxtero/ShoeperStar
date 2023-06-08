9a0dfe3fc29808b4b524b82faa1043f1ac38fd67
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:14:23 +0800

    implemented the brand repository interface

---
+++ ShoeperStar/Data/Repository/BrandRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoeperStar.Data.Contracts;
+using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Repository
+{
+    public class BrandRepository : RepositoryBase<Brand>, IBrandRepository
+    {
+        public BrandRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public void CreateBrand(Brand brand)
+        {
+            Create(brand);
+        }
+
+        public void DeleteBrand(Brand brand)
+        {
+            Delete(brand);
+        }
+
+        public async Task<IEnumerable<Brand>> GetAllBrands(bool trackChanges)
+        {
+            return await FindAll(trackChanges).ToListAsync();
+        }
+
+        public async Task<Brand> GetBrandAsync(int brandId, bool trackChanges)
+        {
+            return await FindByCondition(x => x.Id == brandId, trackChanges).SingleOrDefaultAsync();
+        }
+
+        public void UpdateBrand(Brand brand)
+        {
+            Update(brand);
+        }
+    }
+}
