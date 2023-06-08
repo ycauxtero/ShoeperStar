5706e136308dbfe7d0065f94f4a2af39d68e9552
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:14:54 +0800

    created the category repository interface

---
+++ ShoeperStar/Data/Contracts/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCatergories(bool trackChanges);
+        Task<Category> GetCatergoryAsync(int catergoryId, bool trackChanges);
+        void CreateCatergory(Category catergory);
+        void DeleteCatergory(Category catergory);
+        void UpdateCatergory(Category catergory);
+    }
+}
