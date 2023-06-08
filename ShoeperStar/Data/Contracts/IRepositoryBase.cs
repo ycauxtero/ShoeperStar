ca11ebb980b536bb0df55a13dc818878c5edc07e
ycauxtero <ycauxtero.gmail.com>
2023-06-08 19:54:39 +0800

    created the base repository interface

---
+++ ShoeperStar/Data/Contracts/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> FindAll(bool trackChanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression, bool trackChanges);
+        void Create(T entity);
+        Task CreateMultiple(IEnumerable<T> entities);
+        void Update(T entity);
+        void UpdateMultiple(IEnumerable<T> entities);
+        void Delete(T entity);
+        void DeleteMultiple(IEnumerable<T> entities);
+    }
+    
+}
