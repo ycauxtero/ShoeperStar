7a82e2620ed6460623e3aeb48e19afd675fadae8
ycauxtero <ycauxtero.gmail.com>
2023-06-08 20:18:28 +0800

    created the extensions helper for ef core

---
+++ ShoeperStar/Extensions/EFCoreHelperExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoeperStar.Models;
+
+namespace ShoeperStar.Extensions
+{
+    public static class EFCoreHelperExtensions
+    {
+        public static IQueryable<Shoe> IncludeShoeNavigationFields(this IQueryable<Shoe> shoes)
+        {
+            return shoes
+                    .Include(x => x.Brand)
+                    .Include(x => x.Catergory)
+                    .Include(x => x.Gender)
+                    .Include(x => x.Variants)
+                    .ThenInclude(x => x.Sizes);
+        }
+
+        public static IQueryable<Variant> IncludeVariantNavigationFields(this IQueryable<Variant> variants)
+        {
+            return variants
+                    .Include(x => x.Shoe)
+                    .Include(x => x.Sizes);
+        }
+
+        public static IQueryable<Size> IncludeSizeNavigationFields(this IQueryable<Size> sizes)
+        {
+            return sizes
+                    .Include(x => x.Variant)
+                    .ThenInclude(x => x.Shoe);
+        }
+
+    }
+}
