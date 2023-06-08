781d9a707c44f7b2f9c0dc8b28dc5be4573871c5
ycauxtero <ycauxtero.gmail.com>
2023-06-08 19:19:34 +0800

    added the dbContext file for EFCore

---
+++ ShoeperStar/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoeperStar.Models;
+
+namespace ShoeperStar
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Shoe> Shoes { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Catergories { get; set; }
+        public DbSet<Gender> Genders { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Variant> Variants { get; set; }
+    }
+}
