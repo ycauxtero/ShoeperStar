897d4e75463071d9b62df555fda239f5ec167862
ycauxtero <ycauxtero.gmail.com>
2023-06-10 20:02:24 +0800

    added GetFilteredShoes method

--- ShoeperStar/Data/Contracts/IShoeRepository.cs
+++ ShoeperStar/Data/Contracts/IShoeRepository.cs
@@ -7,7 +7,7 @@ namespace ShoeperStar.Data.Contracts
     {
         Task<IEnumerable<Shoe>> GetAllShoes(bool trackChanges, bool includeNavigationFields = false);
         Task<Shoe> GetShoeAsync(int shoeId, bool trackChanges, bool includeNavigationFields = false);
-        Task<IEnumerable<Shoe>> GetFilteredShoes(bool trackChanges, ShoeFilterVM filter, bool includeNavigationFields = false)
+        Task<IEnumerable<Shoe>> GetFilteredShoes(bool trackChanges, ShoeFilterVM filter, bool includeNavigationFields = false);
         void CreateShoe(Shoe shoe);
         Task<IEnumerable<Shoe>> GetByIds(IEnumerable<int> ids, bool trackChanges, bool includeNavigationFields = false);
         void DeleteShoe(Shoe shoe);
