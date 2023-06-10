ee4f3bb3860a4efa2e828e5f9700fb0c21d67550
ycauxtero <ycauxtero.gmail.com>
2023-06-10 17:26:19 +0800

    added seed data for shoes

--- ShoeperStar/AppDbInitializer.cs
+++ ShoeperStar/AppDbInitializer.cs
@@ -117,6 +117,157 @@ namespace ShoeperStar
             }
         }
 
+        public static void SeedShoes(AppDbContext dbContext)
+        {
+            if (!dbContext.Shoes.Any())
+            {
+                dbContext.Shoes.AddRange(new List<Shoe>()
+                {
+                    new Shoe()
+                    {
+                        BrandId = 1,
+                        CategoryId = 1,
+                        GenderId = 1,
+                        Name = "Kobe 1 Protro",
+                        Price = 10000,
+                        ImageURL = "https://i.pinimg.com/originals/7d/7d/de/7d7dde45e84deed15c79aa357f013b23.jpg",
+                        Variants = new List<Variant>()
+                        {
+                           new Variant()
+                           {
+                               Color = "Purple",
+                               ColorHex = "#A020F0",
+                               Sizes = new List<Size>()
+                               {
+                                   new Size()
+                                   {
+                                       Quantity = 2,
+                                       Value = "US-10",
+                                       SKU = "NK-KB1P-PUR-US10"
+                                   }
+                               }
+                           } 
+                        }
+                    },
+
+                    new Shoe()
+                    {
+                        BrandId = 2,
+                        CategoryId = 3,
+                        GenderId = 2,
+                        Name = "Ultraboost Light",
+                        Price = 6000,
+                        ImageURL = "https://assets.adidas.com/videos/w_600,f_auto,q_auto/2537110c6e0342aaa2a8afb600beaff9_d98c/Ultraboost_Light_Running_Shoes_Black_HQ6339_video.jpg",
+                        Variants = new List<Variant>()
+                        {
+                           new Variant()
+                           {
+                               Color = "Black",
+                               ColorHex = "#000000",
+                               Sizes = new List<Size>()
+                               {
+                                   new Size()
+                                   {
+                                       Quantity = 3,
+                                       Value = "US-8",
+                                       SKU = "ADI-ULL-BLK-US8"
+                                   },
+                                   new Size()
+                                   {
+                                       Quantity = 1,
+                                       Value = "US-7",
+                                       SKU = "ADI-ULL-BLK-US7"
+                                   }
+                               }
+                           },
+                           new Variant()
+                           {
+                               Color = "White",
+                               ColorHex = "#FFFFFF",
+                               Sizes = new List<Size>()
+                               {
+                                   new Size()
+                                   {
+                                       Quantity = 3,
+                                       Value = "US-8",
+                                       SKU = "ADI-ULL-WHT-US8"
+                                   },
+                                   new Size()
+                                   {
+                                       Quantity = 1,
+                                       Value = "US-7",
+                                       SKU = "ADI-ULL-WHT-US7"
+                                   }
+                               }
+                           }
+                        }
+                    },
+
+                    new Shoe()
+                    {
+                        BrandId = 3,
+                        CategoryId = 6,
+                        GenderId = 3,
+                        Name = "Project Rock 3 Slides",
+                        Price = 3500,
+                        ImageURL = "https://cdna.lystit.com/520/650/n/photos/underarmour/b3314b80/under-armour-black-Unisex-project-rock-3-slides.jpeg",
+                        Variants = new List<Variant>()
+                        {
+                           new Variant()
+                           {
+                               Color = "Black",
+                               ColorHex = "#000000",
+                               Sizes = new List<Size>()
+                               {
+                                   new Size()
+                                   {
+                                       Quantity = 2,
+                                       Value = "US-8",
+                                       SKU = "UA-PR3S-BLK-US8"
+                                   },
+                                   new Size()
+                                   {
+                                       Quantity = 5,
+                                       Value = "US-7",
+                                       SKU = "UA-PR3S-BLK-US7"
+                                   }
+                               }
+                           }
+                        }
+                    },
+
+                    new Shoe()
+                    {
+                        BrandId = 4,
+                        CategoryId = 4,
+                        GenderId = 4,
+                        Name = "Chuck Taylor All Star Street",
+                        Price = 6000,
+                        ImageURL = "https://www.conversesandton.co.za/images/large/conversesandton/Lt_Armory_Blue_Blue_White_Kids_Converse-59204-DLQN_ZOOM.jpg",
+                        Variants = new List<Variant>()
+                        {
+                           new Variant()
+                           {
+                               Color = "Morning Glory",
+                               ColorHex = "#97DDE4",
+                               Sizes = new List<Size>()
+                               {
+                                   new Size()
+                                   {
+                                       Quantity = 3,
+                                       Value = "US-4",
+                                       SKU = "CNV-CTAS-BLU-US4"
+                                   }
+                               }
+                           }
+                        }
+                    }
+                });
+
+                dbContext.SaveChanges();
+            }
+        }
+
         public static async Task SeedUsersAndRolesAsync(IApplicationBuilder applicationBuilder)
         {
             using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
