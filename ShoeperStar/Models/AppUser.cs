b34ccfc9541cf49cbb33205ab30ddf4da0d31c1a
ycauxtero <ycauxtero.gmail.com>
2023-06-10 11:25:55 +0800

    added AppUser model for identity

---
+++ ShoeperStar/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ShoeperStar.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DeliveryAddress { get; set; }
+    }
+}
