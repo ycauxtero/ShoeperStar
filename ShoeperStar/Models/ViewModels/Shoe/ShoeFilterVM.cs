90256f06ca2ae902b359fe566bab9bfd305d5168
ycauxtero <ycauxtero.gmail.com>
2023-06-10 19:57:27 +0800

    created shoe filter VM

---
+++ ShoeperStar/Models/ViewModels/Shoe/ShoeFilterVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoeperStar.Models.ViewModels
+{
+    public class ShoeFilterVM
+    {
+        public int BrandFilter { get; set; }
+        public int CategoryFilter { get; set; }
+        public int GenderFilter { get; set; }
+        public string ModelFilter { get; set; }
+        public string SortField { get; set; } = nameof(Shoe.Name);
+        public string SortOrder { get; set; } = "asc";
+    }
+}
