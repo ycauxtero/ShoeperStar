c27fb850afd49a7b138fbe33f982ec7aefd3c09f
ycauxtero <ycauxtero.gmail.com>
2023-06-10 20:00:08 +0800

    added GetFilteredShoes method

--- ShoeperStar/Data/Repository/ShoeRepository.cs
+++ ShoeperStar/Data/Repository/ShoeRepository.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ShoeperStar.Data.Contracts;
+using ShoeperStar.Data.Repository.Query;
 using ShoeperStar.Extensions;
 using ShoeperStar.Models;
+using ShoeperStar.Models.DTO;
+using ShoeperStar.Models.ViewModels;
 
 namespace ShoeperStar.Data.Repository
 {
@@ -21,6 +24,30 @@ namespace ShoeperStar.Data.Repository
             }
 
             return await FindByCondition(x => x.Id.Equals(shoeId), trackChanges).SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Shoe>> GetFilteredShoes(bool trackChanges, ShoeFilterVM filter, bool includeNavigationFields = false)
+        {
+            var filteredEmployeesQuery = FindByCondition(s =>
+                                                            s.Variants.Any(v =>
+                                                                v.Sizes.Any(x => x.Quantity > 0))
+                                                        , trackChanges)
+                                            .SearchShoes(filter.ModelFilter)
+                                            .FilterShoes(new ShoeFilterDTO
+                                            {
+                                                BrandId = filter.BrandFilter,
+                                                CategoryId = filter.CategoryFilter,
+                                                GenderId = filter.GenderFilter
+                                            })
+                                            .SortShoes($"{filter.SortField} {filter.SortOrder}");
+            if (includeNavigationFields)
+            {
+                return await filteredEmployeesQuery
+                            .IncludeShoeNavigationFields()
+                            .ToListAsync();
+            }
+
+            return await filteredEmployeesQuery.ToListAsync();
         }
 
         public async Task<IEnumerable<Shoe>> GetAllShoes(bool trackChanges, bool includeNavigationFields = false)
