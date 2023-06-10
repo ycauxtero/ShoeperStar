ff5ca7b81624c8cd69be2fc5499d679a46ff1954
ycauxtero <ycauxtero.gmail.com>
2023-06-10 17:54:13 +0800

    created strings related extension method helper class

---
+++ ShoeperStar/Data/Services/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace ShoeperStar.Data.Services
+{
+    public static class StringExtensions
+    {
+        public static string ToSlug(this string s)
+        {
+            var stringArray = s.Split(' ');
+            return string.Join('-', stringArray).ToLower();
+        }
+    }
+}
