6ecc358278c4f2f7e8a21180e64ce3396005016e
ycauxtero <ycauxtero.gmail.com>
2023-06-10 11:48:22 +0800

    created service for HashId

---
+++ ShoeperStar/Data/Services/HashIdService.cs
@@ -0,0 +1,41 @@
+﻿using HashidsNet;
+
+namespace ShoeperStar.Data.Services
+{
+    public class HashIdService
+    {
+        private readonly string _salt;
+        private readonly Hashids _hashId;
+
+        public HashIdService(string salt)
+        {
+            _salt = salt;
+            _hashId = new Hashids(_salt);
+        }
+
+        public string EncodeInt(int id)
+        {
+            return _hashId.Encode(id);
+        }
+
+        public int Decode(string hash)
+        {
+
+            try
+            {
+                var id = _hashId.Decode(hash)[0];
+
+                return id;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+    }
+
+    public class HashId
+    {
+        public string Salt { get; set; }
+    }
+}
