75715f32692b0306ce1a06eadfcdf56cd55c3b48
ycauxtero <ycauxtero.gmail.com>
2023-06-10 19:57:51 +0800

    created shoe filter DTO

---
+++ ShoeperStar/Models/DTO/ShoeFilterDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoeperStar.Models.DTO
+{
+    public class ShoeFilterDTO
+    {
+        public int BrandId { get; set; }
+        public int GenderId { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
