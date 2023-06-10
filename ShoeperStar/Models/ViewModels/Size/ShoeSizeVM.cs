daca86a43e3f5a516bea62e4b389ff6d224eef6a
ycauxtero <ycauxtero.gmail.com>
2023-06-10 19:12:00 +0800

    create shoe size view model

---
+++ ShoeperStar/Models/ViewModels/Size/ShoeSizeVM.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoeperStar.Models.ViewModels.Size
+{
+    public class ShoeSizeVM
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        public int Quantity { get; set; }
+        public string SKU { get; set; }
+
+        public int VariantId { get; set; }
+        public ShoeVariantVM Variant { get; set; }
+    }
+}
