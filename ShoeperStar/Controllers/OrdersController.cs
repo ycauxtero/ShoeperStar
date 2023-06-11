932e8542416f0077abb07180b929585fed411105
ycauxtero <ycauxtero.gmail.com>
2023-06-11 17:05:22 +0800

    added dependencies

--- ShoeperStar/Controllers/OrdersController.cs
+++ ShoeperStar/Controllers/OrdersController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ShoeperStar.Data.Contracts;
 
 namespace ShoeperStar.Controllers
 {
     public class OrdersController : Controller
     {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly IMapper _mapper;
+
+        public OrdersController(IRepositoryManager repositoryManager, IMapper mapper)
+        {
+            _repositoryManager = repositoryManager;
+            _mapper = mapper;
+        }
+
         public IActionResult Index()
         {
             return View();
