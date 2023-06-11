3bf9943b52485912490b342b4cf0ec38e441c75a
ycauxtero <ycauxtero.gmail.com>
2023-06-11 17:11:25 +0800

    created Order repository interface

---
+++ ShoeperStar/Data/Contracts/IOrderRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface IOrderRepository
+    {
+        Task<IEnumerable<Order>> GetAllOrdersAsync(bool trackChanges, bool includeNavigationFields = true);
+        Task<IEnumerable<Order>> GetAllOrdersAsync(string userId, bool trackChanges, bool includeNavigationFields = true);
+        Task<Order> GetOrderAsync(Guid id, bool trackChanges, bool includeNavigationFields = true);
+        void CreateOrder(Order order);
+        void CreateOrder(IEnumerable<CartItem> cartItems, string userId);
+        void UpdateOrder(Order order);
+        void UpdateOrders(IEnumerable<Order> orders);
+        void DeleteOrder(Order order);
+    }
+}
