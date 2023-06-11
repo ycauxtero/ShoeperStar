2b8e17b53c6524353574514422a162f3de4af48b
ycauxtero <ycauxtero.gmail.com>
2023-06-11 13:22:56 +0800

    created VariantForCreationVM

---
+++ ShoeperStar/Models/ViewModels/Variant/VariantForCreationVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ShoeperStar.Models.ViewModels
+{
+    public class VariantForCreationVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Color")]
+        [Required]
+        public string Color { get; set; }
+
+        [Display(Name = "Hex Value")]
+        [Required]
+        public string ColorHex { get; set; }
+
+        public int ShoeId { get; set; }
+        public string ShoeModel { get; set; }
+
+    }
+}
