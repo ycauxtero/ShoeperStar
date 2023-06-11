2aa926134cddd3a742917492da424e0554d1d2a4
ycauxtero <ycauxtero.gmail.com>
2023-06-11 18:03:39 +0800

    added controller level authorize attribute

--- ShoeperStar/Controllers/AdminController.cs
+++ ShoeperStar/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ShoeperStar.Data.Contracts;
@@ -8,6 +9,7 @@ using ShoeperStar.Models.ViewModels;
 
 namespace ShoeperStar.Controllers
 {
+    [Authorize]
     [Route("[controller]")]
     public class AdminController : Controller
     {
