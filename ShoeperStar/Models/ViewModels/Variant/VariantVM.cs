864bb53c903d2e0f6eae2b03f252c3d2381e3f4d
ycauxtero <ycauxtero.gmail.com>
2023-06-11 13:22:39 +0800

    create VariantVM

---
+++ ShoeperStar/Models/ViewModels/Variant/VariantVM.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoeperStar.Models.ViewModels
+{
+    public class VariantVM
+    {
+        public int Id { get; set; }
+        public string Color { get; set; }
+        public string ColorHex { get; set; }
+    }
+}
