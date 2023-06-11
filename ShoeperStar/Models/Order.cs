61721f02f429a553f6c6cc0c82772eb7e9538388
ycauxtero <ycauxtero.gmail.com>
2023-06-11 17:07:35 +0800

    created orders related models

---
+++ ShoeperStar/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoeperStar.Models
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime PaymentExpiry { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsShipped { get; set; }
+        public bool OrderRecieved { get; set; }
+        public bool IsCancelled { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public AppUser User { get; set; }
+
+        public ICollection<OrderItem> OrderItems { get; set; }
+    }
+}
---
+++ ShoeperStar/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShoeperStar.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public int SizeId { get; set; }
+        public Size Size { get; set; }
+
+        public double TotalPrice { get; set; }
+        public int Quantity { get; set; }
+
+        public Guid OrderId { get; set; }
+        [ForeignKey(nameof(OrderId))]
+        public Order Order { get; set; }
+    }
+}
