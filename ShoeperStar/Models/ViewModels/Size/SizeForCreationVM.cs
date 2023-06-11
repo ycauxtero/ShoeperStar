99168d097442c8a38be352ed33b8e9b2044049d5
ycauxtero <ycauxtero.gmail.com>
2023-06-11 13:48:51 +0800

    created SizeForCreationVM

---
+++ ShoeperStar/Models/ViewModels/Size/SizeForCreationVM.cs
@@ -0,0 +1,28 @@
+﻿using ShoeperStar.Data.Custom.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ShoeperStar.Models.ViewModels
+{
+    public class SizeForCreationVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Size")]
+        [Required(ErrorMessage = "Please input shoe size")]
+        public string Value { get; set; }
+
+        [MustBeGreaterThanZero]
+        public string Quantity { get; set; }
+
+        [Required(ErrorMessage = "Please input SKU")]
+        public string SKU { get; set; }
+
+        public int ShoeId { get; set; }
+        public int VariantId { get; set; }
+
+        public string ShoeModel { get; set; }
+        public string Color { get; set; }
+        public string ColorHex { get; set; }
+    }
+}
