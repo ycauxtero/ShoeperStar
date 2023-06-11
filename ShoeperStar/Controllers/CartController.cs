e793a8d64bbda33763f84d25053592f7b0689253
ycauxtero <ycauxtero.gmail.com>
2023-06-11 16:12:25 +0800

    added dependencies

--- ShoeperStar/Controllers/CartController.cs
+++ ShoeperStar/Controllers/CartController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoeperStar.Data.Contracts;
+using ShoeperStar.Models;
 
 namespace ShoeperStar.Controllers
 {
     public class CartController : Controller
     {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IMapper _mapper;
+
+
+        public CartController(IRepositoryManager repositoryManager, UserManager<AppUser> userManager, IMapper mapper)
+        {
+            _repositoryManager = repositoryManager;
+            _userManager = userManager;
+            _mapper = mapper;
+        }
         public IActionResult Index()
         {
             return View();
