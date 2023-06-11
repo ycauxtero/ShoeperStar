d4cf6aa28ccf4e2daa13f9e0ea7c88171603c9c9
ycauxtero <ycauxtero.gmail.com>
2023-06-11 17:32:36 +0800

    created Filter action

--- ShoeperStar/Controllers/OrdersController.cs
+++ ShoeperStar/Controllers/OrdersController.cs
@@ -104,6 +104,34 @@ namespace ShoeperStar.Controllers
             return RedirectToAction(nameof(Expired));
         }
 
+        [Authorize(Roles = UserRoles.Admin)]
+        public async Task<IActionResult> Filter(string option, int? page = 1)
+        {
+            option ??= "unpaid-orders";
+
+            var orders = await _repositoryManager.Orders.GetAllOrdersAsync(trackChanges: false);
+            switch (option)
+            {
+                case "orders-for-shipping":
+                    orders = orders.Where(x => x.IsPaid && !x.IsShipped);
+                    break;
+                case "shipped-orders":
+                    orders = orders.Where(x => x.IsShipped && !x.OrderRecieved);
+                    break;
+                case "received-orders":
+                    orders = orders.Where(x => x.OrderRecieved);
+                    break;
+                default:
+                    orders = orders.Where(x => !x.IsCancelled && !x.IsPaid);
+                    break;
+            }
+
+            ViewData["option"] = option;
+            orders = orders.OrderByDescending(x => x.OrderDate);
+            var ordersVM = _mapper.Map<IEnumerable<OrderVM>>(orders);
+
+            return View("Index", ordersVM.ToPagedList((int)page, GetPageSize(null)));
+        }
 
 
 
@@ -168,6 +196,57 @@ namespace ShoeperStar.Controllers
             await _repositoryManager.SaveAsync();
         }
 
+        private async Task<IEnumerable<Size>> GetShoeSizesForStocksUpdate(IEnumerable<Order> expiredOrders)
+        {
+            var shoeSizesToUpdate = expiredOrders
+                                    .SelectMany(order =>
+                                        order.OrderItems.Select(orderItem =>
+                                            new
+                                            {
+                                                id = orderItem.SizeId,
+                                                qty = orderItem.Quantity
+                                            }
+                                            ))
+                                            .GroupBy(x => x.id)
+                                            .Select(x => new
+                                            {
+                                                id = x.Key,
+                                                qty = x.Select(x => x.qty).Sum()
+                                            });
+
+            var shoesSizes = new List<Size>();
+            foreach (var shoeSize in shoeSizesToUpdate)
+            {
+                var movieFromDb = await _repositoryManager.Sizes.GetSizeAsync(shoeSize.id, trackChanges: false, includeNavigationFields: false);
+                movieFromDb.Quantity += shoeSize.qty;
+
+                shoesSizes.Add(movieFromDb);
+            }
+
+            return shoesSizes;
+        }
+
+        private IEnumerable<Order> GetOrdersForCancellationWithoutNavigationFields(IEnumerable<Order> expiredOrders)
+        {
+            var ordersForCancellation = new List<Order>();
+
+            foreach (var order in expiredOrders)
+            {
+                ordersForCancellation.Add(new Order
+                {
+                    Id = order.Id,
+                    OrderDate = order.OrderDate,
+                    PaymentExpiry = order.PaymentExpiry,
+                    UserId = order.UserId,
+                    IsCancelled = true,
+                    IsPaid = order.IsPaid,
+                    IsShipped = order.IsShipped,
+                    OrderRecieved = order.OrderRecieved
+                });
+            }
+
+            return ordersForCancellation;
+        }
 
     }
 }
