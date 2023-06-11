840553b83e20638117f40db5b6f7a5297cb85724
ycauxtero <ycauxtero.gmail.com>
2023-06-11 12:07:14 +0800

    created MustBeGreaterThanZero validation attribute

---
+++ ShoeperStar/Data/Custom/Attributes/MustBeGreaterThanZeroAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeperStar.Data.Custom.Attributes
+{
+    public class MustBeGreaterThanZeroAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext ctx)
+        {
+            string errorMessage;
+
+            if (value is null)
+            {
+                errorMessage = ErrorMessage ?? $"Please input value for {ctx.DisplayName}";
+                return new ValidationResult(errorMessage);
+            }
+
+            var isANumber = double.TryParse(value.ToString(), out double parsedValue);
+
+            if (!isANumber)
+            {
+                errorMessage = ErrorMessage ?? $"Please input a valid {ctx.DisplayName}";
+                return new ValidationResult(errorMessage);
+            }
+
+            if (parsedValue <= 0)
+            {
+                errorMessage = ErrorMessage ?? $"{ctx.DisplayName} must be greater than zero";
+                return new ValidationResult(errorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
