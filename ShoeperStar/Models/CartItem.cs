14eebdba750078c6e42168f42c67b01238b78df0
ycauxtero <ycauxtero.gmail.com>
2023-06-11 16:36:01 +0800

    created CartItem model

---
+++ ShoeperStar/Models/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoeperStar.Models
+{
+    public class CartItem
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public Size Size { get; set; }
+        public int Qty { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
