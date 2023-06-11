a5a22758b57591495ae0249f20e5e5012184a729
ycauxtero <ycauxtero.gmail.com>
2023-06-11 10:01:20 +0800

    created roles dropdown viewComponent

---
+++ ShoeperStar/Data/Custom/ViewComponents/RolesDropDown.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoeperStar.Data.Custom.ViewComponents
+{
+    public class RolesDropDown : ViewComponent
+    {
+        private AppDbContext _dbContext;
+
+        public RolesDropDown(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var roleStore = new RoleStore<IdentityRole>(_dbContext);
+
+            var roles = roleStore.Roles.Select(x => x.Name).ToList();
+            roles.Insert(0, "Select");
+
+            return View(roles);
+        }
+    }
+}
