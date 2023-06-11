8f1c8176e3b224ca6b707cfa4f98f98810e65103
ycauxtero <ycauxtero.gmail.com>
2023-06-11 16:56:31 +0800

    created CartItemVM

---
+++ ShoeperStar/Models/ViewModels/CartItemVM.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoeperStar.Models.ViewModels
+{
+    public class CartItemVM
+    {
+        public uint Id { get; set; }
+
+        public SizeVM Size { get; set; }
+        public int Qty { get; set; }
+        public double Price { get; set; }
+        public string Model { get; set; }
+        public string ColorHex { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
