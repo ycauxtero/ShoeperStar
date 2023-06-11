ff5a00924ccdb31bf16addf0be0785c42f03d65c
ycauxtero <ycauxtero.gmail.com>
2023-06-11 09:02:16 +0800

    setup dependencies

--- ShoeperStar/Controllers/AdminController.cs
+++ ShoeperStar/Controllers/AdminController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ShoeperStar.Data.Contracts;
 
 namespace ShoeperStar.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IRepositoryManager _repositoryManager;
+        private readonly IMapper _mapper;
+
+        public AdminController(IRepositoryManager repositoryManager, IMapper mapper)
+        {
+            _repositoryManager = repositoryManager;
+            _mapper = mapper;
+        }
         public IActionResult Index()
         {
             return View();
