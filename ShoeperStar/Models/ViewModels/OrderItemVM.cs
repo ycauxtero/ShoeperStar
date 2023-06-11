0c2b263cfcb224025fe49e0f1ee6d399890b9bfc
ycauxtero <ycauxtero.gmail.com>
2023-06-11 17:24:42 +0800

    created OrderItemVM

---
+++ ShoeperStar/Models/ViewModels/OrderItemVM.cs
@@ -0,0 +1,13 @@
+﻿namespace ShoeperStar.Models.ViewModels
+{
+    public class OrderItemVM
+    {
+        public long Id { get; set; }
+
+        public int SizeId { get; set; }
+        public ShoeSizeVM Size { get; set; }
+
+        public double TotalPrice { get; set; }
+        public int Quantity { get; set; }
+    }
+}
