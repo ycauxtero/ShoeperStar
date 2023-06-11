824c0a3d870d5a0c1133183f306fb828a9b46c66
ycauxtero <ycauxtero.gmail.com>
2023-06-11 12:06:30 +0800

    created shoeForCreationVM

---
+++ ShoeperStar/Models/ViewModels/Shoe/ShoeForCreationVM.cs
@@ -0,0 +1,34 @@
+﻿using ShoeperStar.Data.Custom.Attributes;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ShoeperStar.Models.ViewModels
+{
+    public class ShoeForCreationVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Model")]
+        [Required(ErrorMessage = "Please input shoe model")]
+        public string Name { get; set; }
+
+        [MustBeGreaterThanZero]
+        public string Price { get; set; }
+
+        [Display(Name = "Image URL")]
+        [Required(ErrorMessage = "Please input image url")]
+        public string ImageURL { get; set; }
+
+        [Display(Name = "Brand")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a brand")]
+        public int BrandId { get; set; }
+
+        [Display(Name = "Gender")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a gender")]
+        public int GenderId { get; set; }
+
+        [Display(Name = "Category")]
+        [Range(1, int.MaxValue, ErrorMessage = "Please select a category")]
+        public int CategoryId { get; set; }
+    }
+}
