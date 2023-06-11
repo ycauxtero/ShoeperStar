e42416820939563e092c26c999bd42940c698081
ycauxtero <ycauxtero.gmail.com>
2023-06-11 09:13:38 +0800

    added dependencies

--- ShoeperStar/Controllers/AccountController.cs
+++ ShoeperStar/Controllers/AccountController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoeperStar.Models;
 
 namespace ShoeperStar.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signInManager;
+        private AppDbContext _dbContext;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, AppDbContext dbContext)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
             return View();
