a357d986cbb2df0b4a503e9da61ed2a7a623278b
ycauxtero <ycauxtero.gmail.com>
2023-06-11 16:50:35 +0800

    create cart items count summary view component

---
+++ ShoeperStar/Data/Custom/ViewComponents/CartSummary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoeperStar.Data.Contracts;
+
+namespace ShoeperStar.Data.Custom.ViewComponents
+{
+    public class CartSummary : ViewComponent
+    {
+        private ICartRepository _cartRepository;
+
+        public CartSummary(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public IViewComponentResult Invoke(string userId)
+        {
+            var userCartItems = _cartRepository.GetCartItems(userId).GetAwaiter().GetResult();
+            var totalCartItemCount = userCartItems.Select(x => x.Qty).Sum();
+
+            return View(totalCartItemCount);
+        }
+    }
+}
