65e5db1ce9f04bf495e15b204b988468108b688a
ycauxtero <ycauxtero.gmail.com>
2023-06-11 16:36:56 +0800

    created interface for CartRepository

---
+++ ShoeperStar/Data/Contracts/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using ShoeperStar.Models;
+
+namespace ShoeperStar.Data.Contracts
+{
+    public interface ICartRepository
+    {
+        Task<IEnumerable<CartItem>> GetCartItems(string userId, bool includeNavigationFields = false);
+        Task<IEnumerable<CartItem>> GetCartItemsBasedOnActualStocks(string userId);
+        Task AddCartItem(CartItem cartItem);
+        Task AddCartItem(int shoeSizeId, string userId);
+        Task DeleteCartItem(CartItem cartItem);
+        Task DeleteCartItems(string userId);
+        void DeleteCartItems(IEnumerable<CartItem> cartItems);
+        void UpdateCartItem(CartItem cartItem);
+    }
+}
