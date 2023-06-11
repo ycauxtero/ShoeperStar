a231e4baa19931c422190c33c3fb4272cfe1444c
ycauxtero <ycauxtero.gmail.com>
2023-06-11 17:24:32 +0800

    created OrderVM

---
+++ ShoeperStar/Models/ViewModels/OrderVM.cs
@@ -0,0 +1,18 @@
+﻿namespace ShoeperStar.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public Guid Id { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime PaymentExpiry { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsShipped { get; set; }
+        public bool OrderRecieved { get; set; }
+        public bool IsCancelled { get; set; }
+
+        public string UserId { get; set; }
+
+        public IEnumerable<OrderItemVM> OrderItems { get; set; }
+    }
+}
