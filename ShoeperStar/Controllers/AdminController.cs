4134336ddc9fa4969488d0b7e30388e2a719ce7e
ycauxtero <ycauxtero.gmail.com>
2023-06-11 09:10:49 +0800

    created Brands action

--- ShoeperStar/Controllers/AdminController.cs
+++ ShoeperStar/Controllers/AdminController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using ShoeperStar.Data.Contracts;
+using ShoeperStar.Models.DTO;
 
 namespace ShoeperStar.Controllers
 {
+    [Route("[controller]")]
     public class AdminController : Controller
     {
         private readonly IRepositoryManager _repositoryManager;
@@ -14,9 +16,17 @@ namespace ShoeperStar.Controllers
             _repositoryManager = repositoryManager;
             _mapper = mapper;
         }
-        public IActionResult Index()
+
+        [Route("[action]")]
+        public async Task<IActionResult> Brands(BaseForNavModelDTO dto)
         {
-            return View();
+            var items = await _repositoryManager.Brands.GetAllBrands(trackChanges: false);
+
+            ViewBag.Title = "All Brands";
+            ViewBag.Items = items;
+            dto.Category = "Brand";
+
+            return View("ShoeFilters", dto);
         }
     }
 }
